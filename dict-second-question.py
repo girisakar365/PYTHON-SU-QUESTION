589b34ce64c0b98f67871c7757ac8112f47f9254
girisakar365 <sakargiri365@gmail.com>
2023-05-10 09:50:38 +0545

    add

---
+++ dict-second-question.py
@@ -0,0 +1,10 @@
+"""
+Create three different dictionaries and then create one dictionary that contains all three dictionaries
+"""
+dict1 = {"One": 1, "Two": 2, "Three": 3}
+dict2 = {"Four": 4, "Five": 5, "Six": 6}
+dict3 = {"Seven": 7, "Eight": 8, "Nine": 3}
+
+main_dict = {"dict1": dict1, "dict2": dict2, "dict3": dict3}
+
+print(main_dict)
